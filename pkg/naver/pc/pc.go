a83756e415624864d792bdd3ee0474e70cea5de6
Goohyun Chung <goohyun.chung@navercorp.com>
2021-11-27 02:33:38 +0900

    pc test is in progress

--- pkg/naver/pc/pc.go
+++ pkg/naver/pc/pc.go
@@ -17,22 +17,22 @@ func (_ pc) GetNewsHomeNewsList(p *rt.PageTemplate, dd types.DumpDirectory) ([]t
 	newsList := make([]types.News, 0)
 
 	//timelateBlock := p.SelectOrPanic("div.home_timelate")
-	containerBlock := p.SelectOrPanic("div#container")
-	mainBlock := containerBlock.SelectOrPanic("div#main_content")
-	asideBlock := containerBlock.SelectOrPanic("div#main_aside")
+	// containerBlock := p.SelectOrPanic("div#container")
+	// mainBlock := containerBlock.SelectOrPanic("div#main_content")
+	// asideBlock := containerBlock.SelectOrPanic("div#main_aside")
 
-	headlineBlock := mainBlock.SelectOrPanic("div#today_main_news")
-	politicsBlock := mainBlock.SelectOrPanic("div#section_politics")
-	economyBlock := mainBlock.SelectOrPanic("div#section_economy")
-	societyBlock := mainBlock.SelectOrPanic("div#section_society")
-	lifeBlock := mainBlock.SelectOrPanic("div#section_life")
-	worldBlock := mainBlock.SelectOrPanic("div#section_world")
-	itBlock := mainBlock.SelectOrPanic("div#section_it")
+	// headlineBlock := mainBlock.SelectOrPanic("div#today_main_news")
+	// politicsBlock := mainBlock.SelectOrPanic("div#section_politics")
+	// economyBlock := mainBlock.SelectOrPanic("div#section_economy")
+	// societyBlock := mainBlock.SelectOrPanic("div#section_society")
+	// lifeBlock := mainBlock.SelectOrPanic("div#section_life")
+	// worldBlock := mainBlock.SelectOrPanic("div#section_world")
+	// itBlock := mainBlock.SelectOrPanic("div#section_it")
 
-	rankingBlock := asideBlock.SelectOrPanic("div.section")
+	// rankingBlock := asideBlock.SelectOrPanic("div.section")
 
-	flickBlock := headlineBlock.SelectOrPanic("div.hdline_flick")
-	articleListBlock := headlineBlock.SelectOrPanic("ul.hdline_article_list")
+	// flickBlock := headlineBlock.SelectOrPanic("div.hdline_flick")
+	// articleListBlock := headlineBlock.SelectOrPanic("ul.hdline_article_list")
 
 	return newsList, nil
 }
